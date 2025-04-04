c0176a39397c0bcf6089d61dd7a32f265d9f8931
marcelo <marcheloalbis@gmail.com>
2025-04-04 02:41:09 -0400

    configuracion de correo

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,10 @@
-import { Controller, Get } from '@nestjs/common';
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import { Controller, Get, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 import { TestService } from './test/test.service';
+import { ApiOperation, ApiQuery } from '@nestjs/swagger';
+import { sendEmail } from './mailer/mailer.helper'; // ⬅️ IMPORTA TU HELPER
 
 @Controller()
 export class AppController {
@@ -10,9 +14,28 @@ export class AppController {
   ) {}
 
   @Get()
+  @ApiOperation({ summary: 'Obtener saludo de prueba' })
   getHello(): string {
-   // this.testService.testLog(); // Aquí se dispara el log
+    // this.testService.testLog(); // Aquí se dispara el log
     return 'Hello World!';
+  }
+
+  @Get('test')
+  @ApiQuery({ name: 'to', required: true })
+  async sendTestEmail(@Query('to') to: string) {
+    const name = 'Marce Tester 🧪';
+    const activationLink = 'https://backend-app.dev/activate/123456';
+   /*  await sendEmail(to, 'Activa tu cuenta', 'account-activated', {
+      name,
+      activationLink,
+    }); */
+    await sendEmail(to, 'Actasdiva tu cuenta', 'reset-password', {
+      name,
+      activationLink,
+    });
+    return {
+      message: `Correo enviado correctamente a ${to}`,
+    };
   }
 
   @Get('error')
