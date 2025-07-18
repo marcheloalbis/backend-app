b9003b2035d3738acdb0119ee0613b24bed6ccd1
marcheloalbis <39276450+marcheloalbis@users.noreply.github.com>
2025-07-18 17:34:51 -0400

    fix: remove self import in AuthModule

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -3,14 +3,14 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtStrategy } from './strategies/jwt.strategy';
 import { MailerModule } from '../mailer/mailer.module';
+import { MailerService } from '../mailer/mailer.service';
 import { CoreModule } from 'src/shared/core.module';
 @Module({
   imports: [
     CoreModule,           // Prisma, Config, Logger
-    AuthModule,
     MailerModule,
   ],
-  providers: [AuthService, JwtStrategy, MailerModule],
+  providers: [AuthService, JwtStrategy, MailerService],
   controllers: [AuthController],
   exports: [AuthService],
 })
